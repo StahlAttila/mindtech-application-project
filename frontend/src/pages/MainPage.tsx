dd8ba5dc6019af236e3eb0d6d7916be6a542795b
StahlAttila <stattilasd@gmail.com>
2021-12-21 17:56:50 +0100

    added Alert message for empty data display

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,5 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { Divider, CircularProgress, Stack, Grid } from '@mui/material'
+import React, { useContext, useEffect } from 'react'
 import FilterData from '../components/FilterData/FilterData'
 import StyledAreaChart from '../components/Charts/StyledAreaChart'
 import StyledPieChart from '../components/Charts/StyledPieChart'
@@ -10,6 +9,7 @@ import useHttp from '../hooks/use-http'
 import Spinner from 'react-bootstrap/Spinner'
 import Container from 'react-bootstrap/Container'
 import { Col, Row } from 'react-bootstrap'
+import Alert from 'react-bootstrap/Alert'
 
 const MainPage: React.FC = () => {
   const { chartType, dateFilter } = useContext(CovidDataContext)
@@ -22,7 +22,7 @@ const MainPage: React.FC = () => {
 
   let ChartContent
 
-  if (status === 'completed' && data != null) {
+  if (status === 'completed' && data != null && data.length > 0) {
     if (chartType === 'area') {
       ChartContent = <StyledAreaChart dataSet={data} />
     } else if (chartType === 'pie') {
@@ -30,6 +30,14 @@ const MainPage: React.FC = () => {
     } else if (chartType === 'radial-bar') {
       ChartContent = <StyledRadialBarChart data={data[data.length - 1]} />
     }
+  } else if (status === 'completed') {
+    ChartContent = <Alert variant="warning">No data to display!</Alert>
+  } else {
+    ChartContent = (
+      <Spinner animation="border" variant="info" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+    )
   }
 
   return (
@@ -39,15 +47,9 @@ const MainPage: React.FC = () => {
           <FilterData />
         </Col>
         <Col>
-          {status === 'pending' && 
-            <Spinner animation="border" variant="info" role="status">
-              <span className="visually-hidden">Loading...</span>
-            </Spinner>
-          }
           {ChartContent}
         </Col>
       </Row>
-      
     </Container>
   )
 }
